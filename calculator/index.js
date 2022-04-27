20b7af249527d0fa6b5067d8904dc7d178b6b2de
Helium Pond <thomasmpont@gmail.com>
2022-04-27 19:17:46 +0200

    Add working display and more functions for calculator
    Create clickable calculator display in index.html.
    Add basic styles and layout in style.css.
    Add working digits and basic operations to index.js.
    Link html buttons to js calculator operations.

--- calculator/index.js
+++ calculator/index.js
@@ -1,22 +1,68 @@
+const FONT_SIZE = 64;
 const output = document.getElementById('output');
 
-let currentOperation = null;
 let ans = '0';
+let currentOperation = (null);
 let current = '';
 
-const operate = function (a, operation, b) {
-    return operation(Number(a), Number(b));
+const operate = (ans, operation, current) => {
+    if (operation === null) {
+        return current || ans;
+    }
+    return operation(+ans, +current);
+}
+
+const updateOutput = (n) => {
+    const digitCount = Math.floor(output.clientWidth / FONT_SIZE * 2);
+
+    let sign = n < 0? '-' : '';
+    n = Math.abs(n);
+    output.textContent = (sign + n).slice(0, digitCount);
 }
 
 const actions = {
-    '+': () => {currentOperation = (a, b) => a + b},
-    '-': () => {currentOperation = (a, b) => a - b},
-    '*': () => {currentOperation = (a, b) => a * b},
-    '/': () => {currentOperation = (a, b) => a / b},
-    '=': () => {
-        if(current === '') return;
-        ans = operate(ans, currentOperation, current);
+    'C': () => {
+        ans = '0';
+        currentOperation = null;
         current = '';
-        output.textContent = ans;
+        updateOutput(0);
+    },
+    '^': () => {currentOperation = (n, exp) => n ** exp},
+    '%': () => {
+        actions['='];
+        ans /= 100;
+        updateOutput(ans);
+    },
+    '+/-': () => {
+        actions['='];
+        ans *= -1;
+        updateOutput(ans);
+    },
+    '/': () => {currentOperation = (a, b) => a / b},
+    'x': () => {currentOperation = (a, b) => a * b},
+    '-': () => {currentOperation = (a, b) => a - b},
+    '+': () => {currentOperation = (a, b) => a + b},
+    '=': () => {
+        if (current === '') return;
+        ans = operate(ans, currentOperation, current);
+        currentOperation = null;
+        current = '';
+        updateOutput(ans);
     }
 };
+
+const buttons = [...document.querySelectorAll('#calculator button')];
+buttons.forEach((button) => {
+    const text = button.textContent;
+    if (Number.isInteger(+text) || text === '.') {
+        button.onclick = () => {
+            current += text;
+            updateOutput(current);
+        }
+    } else {
+        button.onclick = () => {
+            actions['=']();
+            actions[text]();
+        }
+    }
+});
